342519e74cb4896678c0b2a7afeb22a025668388
swamp1820 <swamp1820@yandex.ru>
2024-10-05 21:02:02 +0300

    add object panel

--- NasaChallenge2024/Definitions/Models/SpaceObject.cs
+++ NasaChallenge2024/Definitions/Models/SpaceObject.cs
@@ -4,17 +4,31 @@ public class SpaceObject
 {
     public string Name {get;set;}
 
+    public uint Number {get;set;}
+
      public uint DiscoveryYear { get; set; }
 
     public float OrbitalRadius { get; set; } 
 
     public float OrbitalPeriod { get; set; } 
 
+    public string Status {get;set;}
+
     public SpaceObjectHost Host {get;set;}
+
+    public string DetectedBy {get;set;}
+
+    public string ObservedBy {get;set;}
 
 }
 
 public class SpaceObjectHost {
 
     public string Name {get;set;}
+
+    public string SolarTemp {get;set;}
+
+    public string SolarRadius {get;set;}
+
+    public string SolarMass {get;set;}
 }
