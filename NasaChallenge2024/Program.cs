e2319f428aedcfb75f6670dcb2a11466199f9bcc
Andrei Makarov <amakarov@artec3d.com>
2024-10-06 13:46:41 +0200

    tmp

--- NasaChallenge2024/Program.cs
+++ NasaChallenge2024/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NasaChallenge2024;
+using NasaChallenge2024.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddSingleton<UiIVisibilityService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
